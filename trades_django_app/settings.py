b41ad8aff15a28dc684b5d3cf90401e284e2fa54
Jose Bermudez <jose.bermudez@chargebox.com>
2016-05-09 01:03:50 +0100

    delete celery in settings

--- trades_django_app/settings.py
+++ trades_django_app/settings.py
@@ -39,8 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'trades',
-    'kombu.transport.django',
-    'djcelery',
+    'django_jenkins',
 ]
 
 MIDDLEWARE_CLASSES = [
